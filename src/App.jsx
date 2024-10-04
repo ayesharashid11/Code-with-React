89e57e1343704d31c1bf23828072227aef733fea
Muffin1122 <bscs2211@gmail.com>
2024-10-04 23:34:20 +0500

    style: itnial settings

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+ <> <h2 className='text-2xl text-emerald-700 flex justify-center' >Happy Coding!..</h2>
+ </>
+  )
+}
+
+export default App
