86aef9fb60b393cf93d05e21ad1fde0d8b03e0ab
Muffin1122 <bscs2211@gmail.com>
2024-10-05 13:52:42 +0500

    feat: background changer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [color, setColor] = useState('black');
   return (
- <> <h2 className='text-2xl text-emerald-700 flex justify-center' >Happy Coding!..</h2>
- </>
+    <>
+      <div className='h-screen ' style={{backgroundColor: color}}>
+        <div className='flex justify-center '>
+          <div className='p-2 bg-slate-300 w-60 rounded-full'>
+          <button className='m-2'
+          onClick={()=>setColor('green')} disabled={color === 'green'}>Green</button>
+          <button className='m-2 ' onClick={()=>setColor('red')} 
+          disabled={color === 'red'}>Red</button>
+          <button className='m-2 ' onClick={()=>setColor('yellow')} 
+            disabled={color === 'yellow'}>Yellow</button>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
