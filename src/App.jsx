1decf02151313ede1f95a25d5f04939fbd5207c7
Muffin1122 <bscs2211@gmail.com>
2024-10-06 23:15:56 +0500

    refactor files

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import React, { useState } from 'react'
-import './App.css'
-
+import React from 'react'
+import BgChanger from './Components/BgChanger';
 function App() {
-  const [color, setColor] = useState('black');
+  
   return (
-    <>
-      <div className='h-screen ' style={{backgroundColor: color}}>
-        <div className='flex justify-center '>
-          <div className='p-2 bg-slate-300 w-60 rounded-full'>
-          <button className='m-2'
-          onClick={()=>setColor('green')} disabled={color === 'green'}>Green</button>
-          <button className='m-2 ' onClick={()=>setColor('red')} 
-          disabled={color === 'red'}>Red</button>
-          <button className='m-2 ' onClick={()=>setColor('yellow')} 
-            disabled={color === 'yellow'}>Yellow</button>
-          </div>
-        </div>
-      </div>
-    </>
+  <>
+  <BgChanger/>
+  </>
   )
 }
 
