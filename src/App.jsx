0f3ac089d04489c3adc66498656dd8f9559d05b0
Muffin1122 <bscs2211@gmail.com>
2024-10-13 19:25:55 +0500

    style: Currency input box styles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,49 @@
 
 import './App.css'
+import InputBox from './Components/InputBox'
 
 function App() {
-  
+
   return (
- <> <h2 className='text-2xl text-emerald-700 flex justify-center' >Happy Coding!..</h2>
- </>
+    <>
+      <div className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+        style={{
+          backgroundImage: `url('/finance.jpg')`,
+        }}>
+        <div className="w-full">
+          <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+            <form
+
+            >
+              <div className="w-full mb-1">
+                <InputBox
+                  
+                />
+              </div>
+              <div className="relative w-full h-0.5">
+                <button
+                  type="button"
+                  className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md text-white px-2 py-0.5" 
+                  style={{background: '#D08522'}}
+                 
+                >
+                  swap
+                </button>
+              </div>
+              <div className="w-full mt-1 mb-4">
+                <InputBox
+              
+                />
+              </div>
+              <button type="submit" className="w-full text-white px-4 py-3 rounded-lg" style={{background: '#D08522'}}>
+                Convert 
+              </button>
+            </form>
+           
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
